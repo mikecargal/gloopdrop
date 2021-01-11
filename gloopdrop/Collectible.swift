1dc2e42a13a7bfb16519d268619b1d495cfaceec
Mike Cargal <mike@cargal.net>
2021-01-11 11:39:32 -0500

    add drop sounds

--- gloopdrop/Collectible.swift
+++ gloopdrop/Collectible.swift
@@ -13,12 +13,18 @@ enum CollectibleType: String {
     case gloop
 }
 
-class Collectible:SKSpriteNode {
+class Collectible: SKSpriteNode {
     // MARK: - PROPERTIES
+
     private var collectibleType: CollectibleType = .none
     
+    private let playCollectSound = SKAction.playSoundFileNamed("collect.wav", waitForCompletion: false)
+    
+    private let playMissSound = SKAction.playSoundFileNamed("miss.wav", waitForCompletion: false)
+    
     // MARK: - INIT
-    init(collectibleType:CollectibleType) {
+
+    init(collectibleType: CollectibleType) {
         var texture: SKTexture!
         self.collectibleType = collectibleType
         
@@ -36,7 +42,7 @@ class Collectible:SKSpriteNode {
         self.zPosition = Layer.collectible.rawValue
         
         // add [hysics body
-        self.physicsBody = SKPhysicsBody(rectangleOf: self.size, center: CGPoint(x: 0.0, y: -self.size.height/2))
+        self.physicsBody = SKPhysicsBody(rectangleOf: self.size, center: CGPoint(x: 0.0, y: -self.size.height / 2))
         self.physicsBody?.affectedByGravity = false
         
         self.physicsBody?.categoryBitMask = PhysicsCategory.collectible
@@ -44,35 +50,38 @@ class Collectible:SKSpriteNode {
         self.physicsBody?.collisionBitMask = PhysicsCategory.none
     }
     
-    required init?(coder aDecoder:NSCoder) {
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - FUNCTIONS
-    func drop(dropSpeed: TimeInterval, floorLevel:CGFloat) {
+
+    func drop(dropSpeed: TimeInterval, floorLevel: CGFloat) {
         let pos = CGPoint(x: position.x, y: floorLevel)
         
         let scaleX = SKAction.scaleX(to: 1.0, duration: 1.0)
         let scaleY = SKAction.scaleY(to: 1.3, duration: 1.0)
-        let scale = SKAction.group([scaleX,scaleY])
+        let scale = SKAction.group([scaleX, scaleY])
         
         let appear = SKAction.fadeAlpha(to: 1.0, duration: 0.25)
         let moveAction = SKAction.move(to: pos, duration: dropSpeed)
         let actionSequence = SKAction.sequence([
-                                                appear,
-                                                scale,
-                                                moveAction])
+            appear,
+            scale,
+            moveAction,
+        ])
         
-        //Shrink first, then run fall action
+        // Shrink first, then run fall action
         self.scale(to: CGSize(width: 0.25, height: 1.0))
-        self.run(actionSequence,withKey: GloopActionKeys.drop.rawValue)
+        self.run(actionSequence, withKey: GloopActionKeys.drop.rawValue)
     }
     
     func collected() {
-        self.run(SKAction.removeFromParent())
+        self.run(SKAction.group([self.playCollectSound, SKAction.removeFromParent()]))
     }
     
     func missed() {
-        self.run(SKAction.removeFromParent())
+        self.run(SKAction.group([self.playMissSound, SKAction.removeFromParent()]))
     }
 }
